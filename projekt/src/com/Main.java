41f4c0d3623d9a1f73ccf4dab2610840225b426a
frantisek.koci <fanda.kom@gmail.com>
2021-10-21 15:30:54 +0200

    Zacatek ve skole

---
+++ projekt/src/com/Main.java
@@ -0,0 +1,16 @@
+package com;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int [] hra = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    public static void main(String[] args){
+        Methods.printBoard();
+        System.out.println("Player X turn");
+        Methods.Xturn();
+    }
+
+
+
+}
---
+++ projekt/src/com/Methods.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Methods {
+
+    public static void printBoard(){
+        System.out.println(Main.hra[0] + " | " + Main.hra[1] + " | " + Main.hra[2]);
+        System.out.println(Main.hra[3] + " | " + Main.hra[4] + " | " + Main.hra[5]);
+        System.out.println(Main.hra[6] + " | " + Main.hra[7] + " | " + Main.hra[8]);
+    }
+    public static void Xturn(){
+
+    }
+}
